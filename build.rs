a7f411a9f0119527ca2d08c6059aa91abfd00656
fcmlrw <12997336+fcmlrw@user.noreply.gitee.com>
2026-07-12 21:38:07 +0800

    添加icon

---
+++ build.rs
@@ -0,0 +1,9 @@
+fn main() {
+    if cfg!(target_os = "windows") {
+        let mut res = winresource::WindowsResource::new();
+
+        res.set_icon("assets/icons/icon.ico");
+
+        res.compile().unwrap();
+    }
+}
